47cff088e47acd87a517955777dee8f2086db991
sunnyday90210 <robertjohnwollny@gmail.com>
2018-03-25 00:36:00 -0700

    added google auth and set up my passport router

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://robert:robert@ds223509.mlab.com:23509/devtrack",
+  googleClientID:
+    "810977006643-efbea7ick1oajeqmpte8k7li4bppramq.apps.googleusercontent.com",
+  googleClientSecret: "nMnJYvctf_de9GOfCVRuvJfz"
+};
