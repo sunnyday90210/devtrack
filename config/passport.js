5b7654e73bc4e87319eb3add22a9629981070c2c
sunnyday90210 <robertjohnwollny@gmail.com>
2018-03-26 02:41:45 -0700

    finished adding google auth

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("./keys");
+// Load user model
+const User = mongoose.model("users");
 
 module.exports = function(passport) {
   passport.use(
@@ -12,9 +14,42 @@ module.exports = function(passport) {
         proxy: true
       },
       (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-        console.log(profile);
+        // console.log(accessToken);
+        // console.log(profile);
+
+        const image = profile.photos[0].value.substring(
+          0,
+          profile.photos[0].value.indexOf("?")
+        );
+
+        const newUser = {
+          googleID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          image: image
+        };
+
+        User.findOne({
+          googleID: profile.id
+        }).then(user => {
+          if (user) {
+            // Return user
+            done(null, user);
+          } else {
+            // Create user
+            new User(newUser).save().then(user => done(null, user));
+          }
+        });
       }
     )
   );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+  });
 };
