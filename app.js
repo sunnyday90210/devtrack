4b5f644a73ec0de550891a213b8ebe8baa8602f0
sunnyday90210 <robertjohnwollny@gmail.com>
2018-03-27 03:18:04 -0700

    started on the frontend

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
@@ -11,6 +12,7 @@ require("./models/user");
 require("./config/passport")(passport);
 
 // Load Routes
+const index = require("./routes/index");
 const auth = require("./routes/auth");
 
 // Load Keys
@@ -29,9 +31,14 @@ mongoose
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("It works");
-});
+// Handlebars Middleware
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
 app.use(cookieParser());
 app.use(
@@ -53,6 +60,7 @@ app.use((req, res, next) => {
 });
 
 // Use the routes
+app.use("/", index);
 app.use("/auth", auth);
 
 const port = process.env.PORT || 5000;
